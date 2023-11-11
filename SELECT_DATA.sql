876c1a01bbdae976aafd9aef536383cfac6a9a4a
fajarriskihidayat <fajarrh2002@gmail.com>
2023-11-11 22:30:08 +0700

    add select data syntax

---
+++ SELECT_DATA.sql
@@ -0,0 +1,11 @@
+-- mengambil seluruh data dari tabel 
+SELECT * FROM users;
+SELECT * FROM products;
+SELECT * FROM transactions;
+SELECT * FROM transaction_detail;
+
+-- mnegambil data berdasarkan id 
+SELECT * FROM users WHERE id = 2;
+SELECT * FROM products WHERE id = 4;
+SELECT * FROM transactions WHERE id = 6;
+SELECT * FROM transaction_detail WHERE id = 8;
